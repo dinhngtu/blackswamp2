31571107aa176bdba909ae36ce005e9ab704e99b
Tu Dinh <dinhngtu@users.noreply.github.com>
2021-04-04 16:35:09 +0200

    article language setting

--- renderer/PageComponent.tsx
+++ renderer/PageComponent.tsx
@@ -4,7 +4,7 @@ import ArticleComponent from "./ArticleComponent";
 export default function PageComponent(props: { article: Article }) {
   const pageTitle = (props.article.Title ? `${props.article.Title} - ` : "") + "tudinh.xyz";
   return (
-    <html lang="en">
+    <html lang={props.article.Language || "en"}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
