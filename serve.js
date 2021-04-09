471d0a312c2f2319993683ac12e0e9103b0ec48d
Tu Dinh <dinhngtu@users.noreply.github.com>
2021-04-09 13:48:12 +0200

    fix serve.js 404 root

--- serve.js
+++ serve.js
@@ -37,7 +37,7 @@ http.createServer(function (request, response) {
   fs.readFile(filePath, function (error, content) {
     if (error) {
       if (error.code == 'ENOENT') {
-        fs.readFile('./404.html', function (_error, content) {
+        fs.readFile(root + '/404.html', function (_error, content) {
           response.writeHead(404, { 'Content-Type': 'text/html' });
           response.end(content, 'utf-8');
         });
