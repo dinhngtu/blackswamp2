6a02748a75030f0e30b1ef12bce8d7713e804c44
Tu Dinh <dinhngtu@users.noreply.github.com>
2021-05-14 03:09:15 +0200

    minimize static config

--- rollup.static.config.js
+++ rollup.static.config.js
@@ -1,7 +1,4 @@
 import ts from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import replace from '@rollup/plugin-replace';
 import externals from 'rollup-plugin-node-externals';
 import typescript from "typescript";
 
@@ -16,12 +13,6 @@ export default {
       typescript,
       tsconfig: "tsconfig.static.json"
     }),
-    externals(),
-    nodeResolve(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-      preventAssignment: true
-    }),
-    commonjs({ sourceMap: false })
+    externals()
   ]
 };
