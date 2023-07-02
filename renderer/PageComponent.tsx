fcd9631b3156f494afe248fd4d6e0ed192e511de
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2023-07-02 02:56:04 +0200

    implement resource cache buster

--- renderer/PageComponent.tsx
+++ renderer/PageComponent.tsx
@@ -1,16 +1,34 @@
 import { Article } from "./Article";
 import { titleSuffix } from "./Config";
 import PageBodyComponent from "./PageBodyComponent";
+import crypto from "node:crypto";
+import fs from "node:fs";
+import path from "node:path";
+
+let cacheBusterCache: { [key: string]: string } = {};
+function cacheBuster(rel: string) {
+  if (rel in cacheBusterCache) {
+    return cacheBusterCache[rel];
+  }
+  const realPath = path.join("./public", rel);
+  const fileContents = fs.readFileSync(realPath);
+  const h = crypto.createHash("sha256");
+  h.update(fileContents);
+  const digest = h.digest("base64url");
+  const ret = `${rel}?v=${digest.substring(0, 8)}`;
+  cacheBusterCache[rel] = ret;
+  return ret;
+}
 
 function renderModule(mod: string) {
   const css = /^CSS\.([a-zA-Z0-9]+)$/.exec(mod);
   if (css?.length === 2) {
-    return <link rel="stylesheet" href={`/css/${css[1]}.css`} />;
+    return <link rel="stylesheet" href={cacheBuster(`/css/${css[1]}.css`)} />;
   }
 
   const js = /^JS\.([a-zA-Z0-9]+)$/.exec(mod);
   if (js?.length === 2) {
-    return <script src={`/js/${js[1]}.js`} defer />;
+    return <script src={cacheBuster(`/js/${js[1]}.js`)} defer />;
   }
 }
 
@@ -24,8 +42,8 @@ export default function PageComponent(props: { article: Article }) {
         {props.article.Description && <meta name="description" content={props.article.Description} />}
         <title>{pageTitle}</title>
         <link rel="icon" href="data:," />
-        <link rel="stylesheet" href="/css/site.css" />
-        <link rel="stylesheet" href="/css/article.css" />
+        <link rel="stylesheet" href={cacheBuster("/css/site.css")} />
+        <link rel="stylesheet" href={cacheBuster("/css/article.css")} />
         {props.article.Modules?.map(mod => renderModule(mod))}
       </head>
       <body id="root">
